43449b7812dda194e53908220ffdb7fdf505e575
Mario Demetrio <mariodemetrio001@gmail.com>
2020-05-31 12:35:53 +0000

    Altera o nome buscado para Mario

--- hw-rstudio-project.R
+++ hw-rstudio-project.R
@@ -2,7 +2,7 @@ library(magrittr); library(ggplot2)
 
 source("R/utils.R")
 
-x <- "Francisco"
+x <- "Mario"
 
 df <- get_freq_nome(x)
 
@@ -14,3 +14,4 @@ df %>%
   xlab("Década de nascimento") +
   ylab("Pessoas") +
   ggsave("figures/nomes-decada.pdf")
+  pdf("figures/nomes-decada.pdf")
